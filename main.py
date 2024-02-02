399b0a9e6db6232e079a2c31fa83cfc439f10e2f
rus-ai <russian-ai@yandex.ru>
2024-02-02 20:28:00 +0300

    main parameters cleanup

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from telegram.ext import Application
-from telegram import Update
 
 from handlers import setup_dispatcher
 from settings import TELEGRAM_TOKEN
 
 application = Application.builder().token(TELEGRAM_TOKEN).build()
 setup_dispatcher(application)
-application.run_polling(allowed_updates=Update.ALL_TYPES)
+application.run_polling()
 
