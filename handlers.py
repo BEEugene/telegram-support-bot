d0204ee38e7022ea0dc5a65ebb7cd1f76efab550
rus-ai <russian-ai@yandex.ru>
2024-02-02 18:46:29 +0300

    Update async python-telegram-bot==20.7

--- handlers.py
+++ handlers.py
@@ -1,23 +1,26 @@
 import os
-from telegram.ext import CommandHandler, MessageHandler, Filters
+
+from telegram import Chat
+from telegram.ext import CommandHandler, MessageHandler, filters
 
 from settings import WELCOME_MESSAGE, TELEGRAM_SUPPORT_CHAT_ID, REPLY_TO_THIS_MESSAGE, WRONG_REPLY
 
 
-def start(update, context):
-    update.message.reply_text(WELCOME_MESSAGE)
+async def start(update, context):
+    await update.message.reply_text(WELCOME_MESSAGE)
 
     user_info = update.message.from_user.to_dict()
+    chat_info = ""
+    if update.message.chat.type != Chat.PRIVATE:
+        chat_info = f"\n\nChat{update.message.chat.to_dict()}"
 
-    context.bot.send_message(
+    await context.bot.send_message(
         chat_id=TELEGRAM_SUPPORT_CHAT_ID,
-        text=f"""
-📞 Connected {user_info}.
-        """,
+        text=f"Connected {user_info}.{chat_info}"
     )
 
 
-def forward_to_chat(update, context):
+async def forward_to_chat(update, context):
     """{ 
         'message_id': 5, 
         'date': 1605106546, 
@@ -25,16 +28,16 @@ def forward_to_chat(update, context):
         'text': 'TEST QOO', 'entities': [], 'caption_entities': [], 'photo': [], 'new_chat_members': [], 'new_chat_photo': [], 'delete_chat_photo': False, 'group_chat_created': False, 'supergroup_chat_created': False, 'channel_chat_created': False, 
         'from': {'id': 49820636, 'first_name': 'Daniil', 'is_bot': False, 'last_name': 'Okhlopkov', 'username': 'danokhlopkov', 'language_code': 'en'}
     }"""
-    forwarded = update.message.forward(chat_id=TELEGRAM_SUPPORT_CHAT_ID)
+    forwarded = await update.message.forward(chat_id=TELEGRAM_SUPPORT_CHAT_ID)
     if not forwarded.forward_from:
-        context.bot.send_message(
+        await context.bot.send_message(
             chat_id=TELEGRAM_SUPPORT_CHAT_ID,
             reply_to_message_id=forwarded.message_id,
             text=f'{update.message.from_user.id}\n{REPLY_TO_THIS_MESSAGE}'
         )
 
 
-def forward_to_user(update, context):
+async def forward_to_user(update, context):
     """{
         'message_id': 10, 'date': 1605106662, 
         'chat': {'id': -484179205, 'type': 'group', 'title': '☎️ SUPPORT CHAT', 'all_members_are_administrators': True}, 
@@ -53,27 +56,26 @@ def forward_to_user(update, context):
     }"""
     user_id = None
     if update.message.reply_to_message.forward_from:
-        user_id = update.message.reply_to_message.forward_from.id
+        user_id = await update.message.reply_to_message.forward_from.id
     elif REPLY_TO_THIS_MESSAGE in update.message.reply_to_message.text:
         try:
             user_id = int(update.message.reply_to_message.text.split('\n')[0])
         except ValueError:
             user_id = None
     if user_id:
-        context.bot.copy_message(
+        await context.bot.copy_message(
             message_id=update.message.message_id,
             chat_id=user_id,
             from_chat_id=update.message.chat_id
         )
     else:
-        context.bot.send_message(
+        await context.bot.send_message(
             chat_id=TELEGRAM_SUPPORT_CHAT_ID,
             text=WRONG_REPLY
         )
 
 
-def setup_dispatcher(dp):
-    dp.add_handler(CommandHandler('start', start))
-    dp.add_handler(MessageHandler(Filters.chat_type.private, forward_to_chat))
-    dp.add_handler(MessageHandler(Filters.chat(TELEGRAM_SUPPORT_CHAT_ID) & Filters.reply, forward_to_user))
-    return dp
+def setup_dispatcher(app):
+    app.add_handler(CommandHandler('start', start))
+    app.add_handler(MessageHandler(filters.ChatType.PRIVATE, forward_to_chat))
+    app.add_handler(MessageHandler(filters.Chat(TELEGRAM_SUPPORT_CHAT_ID) & filters.REPLY, forward_to_user))
